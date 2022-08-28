16cbaed116d772c41fba9e476785f8d031ea2e40
banchiec <icaro_19@hotmail.com>
2022-08-28 22:19:18 +0200

    Solved commit

---
+++ src/components/Footers/FooterMobileLanding/FooterMobileLanding.js
@@ -0,0 +1,10 @@
+import { ContainerFooterMobile } from "./FooterMobileLandingStyled"
+
+const FooterMobileLanding = () => {
+	return (
+		<ContainerFooterMobile>
+			FooterMobile
+		</ContainerFooterMobile>
+	)
+}
+export default FooterMobileLanding
